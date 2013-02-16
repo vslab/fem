184ea6b8738b3f6f266d65f653f515dae067eed1
Leonardo Bartoloni <bartolon@di.unipi.it>
2013-02-15 18:34:55 -0800

    new implementation of random variables as distribution wrappers

---
+++ distr/RandomVariables.fs
@@ -0,0 +1,49 @@
+﻿module RandomVariables
+open Distributions
+
+type GenId = int
+
+type RandomVariable =
+    | FromDist of obj Distribution * GenId
+    | Always of obj
+    | Bind of RandomVariable * (obj -> RandomVariable)
+
+type RGen<'T> =
+    | R of RandomVariable
+
+type RandomVariableBuilder() =
+    let lastId:GenId ref = ref (LanguagePrimitives.GenericZero)
+
+    member x.Bind (a:RGen<'U>,f:'U ->RGen<'T>) =
+        RGen<'T>.R (Bind(match a with R b -> b,fun (x:obj) -> match f (x :?> 'U) with R v -> v))
+
+    member x.Bind (a:Distribution<'U>,f:'U -> RGen<'T>) = 
+        let id = System.Threading.Interlocked.Increment(lastId)
+        if id = LanguagePrimitives.GenericZero then raise (new System.Exception("Maximum number of variable reached"))
+        let dobj = dist{let! q = a in return q :> obj}
+        let fdist = FromDist(dobj ,id)
+        x.Bind(RGen<'U>.R(fdist),f)
+
+    member x.Return (v: 'T) =
+        RGen<'T>.R (Always v)
+
+    static member getEnvDist (x:RandomVariable) (env: Map<GenId,obj>) =
+        match x with
+        | Always v -> dist { return v, env } 
+        | FromDist (d,id) -> dist {
+            match Map.tryFind id env with
+            | Some y -> return y,env
+            | None -> let! v = d in return v,env.Add(id,v)
+            } 
+        | Bind (d,f) -> dist{
+            let! va,ea = RandomVariableBuilder.getEnvDist d env
+            return! RandomVariableBuilder.getEnvDist (f (va)) ea
+        }
+ 
+let random = new RandomVariableBuilder()
+
+let getDist (v:RGen<'T> ) = 
+    dist {
+        let! vx,ex = RandomVariableBuilder.getEnvDist (match v with R w -> w) Map.empty
+        return vx :?>'T
+        }
---
+++ fem/Main.fs
@@ -0,0 +1,35 @@
+﻿module Main
+
+open Distributions
+open RandomVariables
+
+let d1 = dist { return true }
+let d2 = dist { return 1 }
+(*let r1 = random {
+    let! a = d1
+    let! b = d2
+    return if a then b else 2
+    }*)
+
+let f2 a b =
+    let value = if a then b else 2
+    random.Return(value)
+let f1 a =
+    random.Bind(d2,f2 a)
+(*
+let r1 = random.Bind(d1, f1)
+*)
+let r3 = dist { return true }
+let r4 = dist { return 1 }
+let r2 = random { let! a = r3 in if a then let! b = r4 in return 1 else return 0 }
+let d = getDist r2
+let g = gen()
+
+[<EntryPoint>]
+let main args =
+    let v = d.Value(g)
+    printfn ("%d") (v)
+    System.Console.WriteLine  (v)
+    System.Diagnostics.Debug.WriteLine (v)
+    v
+
